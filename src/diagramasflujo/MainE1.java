2f03b4bfdd7ee40adf760aaea5f6587208221c25
GM REPRESENT <GM REPRESENT@192.168.1.6>
2021-07-07 19:51:55 -0500

    MainE1

--- src/diagramasflujo/MainE1.java
+++ src/diagramasflujo/MainE1.java
@@ -1,10 +1,25 @@
 package diagramasflujo;
 
+import java.util.Scanner;
+
 public class MainE1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);// TECLADO
 
+		System.out.println("Ingrese valor del sueldo:");
+		int S = teclado.nextInt();
+
+		if (S < 300) {
+			// SI
+			int B = 40;
+			S = 0;
+			System.out.println("Su bonificación es: " + S);
+		} else
+			// NO
+			System.out.println("No hay bonificación: B = 0");
+		
 	}
 
 }
