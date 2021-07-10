041f1d8220297dfd81ad907b10ea756493189b72
GM REPRESENT <GM REPRESENT@192.168.1.6>
2021-07-09 19:41:45 -0500

    Trabajo Clase

---
+++ src/diagramas/de/flujo/Main3.java
@@ -0,0 +1,24 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class Main3 {
+	//Imprimir la tabla del 7 multiplicado hasta el 12
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner teclado = new Scanner(System.in);// TECLADO
+
+		System.out.println("Tabla del 7");
+		System.out.println();
+		System.out.println();
+		int N=7;
+		
+		for (int M=0; M<=12; M++) {
+			int R = N*M;
+			System.out.println("La Tabla: " + R);
+		}
+
+	}
+
+}
