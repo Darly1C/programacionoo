ea43a89fa283938b981ee7c77f9708a5e1c4466c
GM REPRESENT <GM REPRESENT@192.168.1.6>
2021-07-21 20:48:37 -0500

    Taller 21 de julio

---
+++ src/tipos/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,20 @@
+package tipos.de.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Taller
+		short cantidadEstudiantes = 1000;
+		byte entradaCircuito = 0; // puede ser 1 o 0
+		int cantidadCiudadanos = 100000;
+		long cantidadMundo = 1000000000;
+		float sueldo = (float) 1000.56;
+		float carro = 1000.56f;
+		float costoCarro = 1000.56F;
+		double saldoBanco = 10000000.67;
+		char opcionMenu = 'C';
+		boolean casado = true;
+	}
+
+}
